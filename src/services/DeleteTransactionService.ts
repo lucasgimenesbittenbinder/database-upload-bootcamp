f8113ac4817cc95b01b8b6f71a2fb1898114a0ec
Lucas Gimenes <lucas.gimenesbittenbinder@gmail.com>
2020-11-04 20:30:24 -0300

    challenge done

---
+++ src/services/DeleteTransactionService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from 'typeorm';
+import { RelationCountAttribute } from 'typeorm/query-builder/relation-count/RelationCountAttribute';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
